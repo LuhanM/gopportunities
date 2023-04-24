bbab0c4d43e0137223d4cf655ab7172b4ba0d736
root <meireles.luhan@gmail.com>
2023-04-24 20:13:35 -0300

    update on validate method of CreateOpeningRequest

--- handler/request.go
+++ handler/request.go
@@ -24,13 +24,13 @@ func (r *CreateOpeningRequest) Validate() error {
 	if r.Role == "" {
 		return errParamIsRequired("role", "string")
 	}
-	if r.Role == "" {
+	if r.Company == "" {
 		return errParamIsRequired("company", "string")
 	}
-	if r.Role == "" {
+	if r.Location == "" {
 		return errParamIsRequired("location", "string")
 	}
-	if r.Role == "" {
+	if r.Link == "" {
 		return errParamIsRequired("link", "string")
 	}
 	if r.Remote == nil {
@@ -40,4 +40,21 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("at least one valid field must be provided")
 }
